310f13cee95ee4070c5f3da77ca2bec361d670ef
JCGreen03 <160683432+JCGreen03@users.noreply.github.com>
2025-05-09 22:34:23 -0400

    Delete Week-8 directory

--- Week-8/CMS-Database/Components/DonateBTN.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "<h2>Set up a recurring donation</h2>
-<p>Can't volunteer? Set up a recurring donation to one of our mission teams or sponsored charities</p>
-<a href='Donate.php' class='button'>View our donation options</a>";
-?>
--- Week-8/CMS-Database/Components/employeeInfo.php
+++
@@ -1,7 +0,0 @@
-<?php
-    echo "Name: " . $rows['FirstName'] . ", ".  $rows['LastName'] . "<br>";
-    echo "Rank: " . $rows['Role'] . "<br>";
-    echo "Email: " . $rows['Email'] . "<br>";   
-    echo "Favorite Book: " . $rows['FavoriteBook'] . "<br>";
-
-?>
